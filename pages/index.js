e6ad55926ebeea2dfa0d6afbae7bc36078b4e704
knights1210 <jamis.1984@gmail.com>
2021-06-29 20:55:09 +0900

    test2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+// import styles from "../styles/Home.module.css";
 import Head from "../component/Head";
 import NavBar from "../component/NavBar";
 
